f169ca7965230ccba3137d42d538cb663a5c511c
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-09-19 08:40:11 +0100

    constructor

--- app/src/main/java/com/example/wordcount/WordCount.java
+++ app/src/main/java/com/example/wordcount/WordCount.java
@@ -10,23 +10,26 @@ public class WordCount {
     int WordCount ;
     String string;
 
-    public static int WordCount(String string)
-         int wordCount = new WordCount
+        public static int WordCount(String string);
+    int wordCount = new WordCount;
 
 
-    {
-        int count = 1;
-        for (int i = 0, i <=string.length() -1; i++)
-        {
-            if (string.charAt(i) == ' ' && string.charAt(i + 1) != ' ')
-            {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    {   String wordCount = in.nextLine();
-    }
 }
 
+
+//
+//
+//    {
+//            int count = 1;
+//            for (int i = 0, i <= string.length() -1; i++);
+//            {
+//            if (string.charAt(i) == ' ' && string.charAt(i + 1) != ' ')
+//            {
+//            count++;
+//            }
+//            }
+//            return count;
+//            }
+//
+//            {   String wordCount = in.nextLine();
+//            }
